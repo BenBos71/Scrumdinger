c44be46726e53992deba434231d669f4252aef79
BenBos71 <ben.bos1097@gmail.com>
2023-05-20 17:22:06 -0600

    Initial Commit. Lesson 1

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ben Bos on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
